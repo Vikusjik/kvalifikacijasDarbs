ea3eaed8c8e3ba96fd0d026dcddd749dd62cb514
Vika <viktoorijaaaaaa@gmail.com>
2024-11-15 00:17:43 +0300

    Auth works

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\User;
+
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    public function index(){
+
+        if(Auth::user()->usertype == 'skolens')
+        {
+            return('dashboard');
+        }
+        else{
+            return view('skolotajs.home');
+        }
+
+
+    }
+}
