a821e4fb5af791608e5ead2b685c550d392241e6
Vika <viktoorijaaaaaa@gmail.com>
2024-11-26 18:51:55 +0200

    Kļūdu labosana migracijas

--- database/migrations/2024_11_26_164138_create_students_table.php
+++ database/migrations/2024_11_26_164138_create_students_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
-            $table->id('name');
+            $table->string('name');
             $table->timestamps();
         });
     }
